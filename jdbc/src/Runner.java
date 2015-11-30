d144a6c29cb8247b33232ca9c45713b5dceac0b1
suntorialux <suntorialux@gmail.com>
2015-12-01 01:19:33 +0400

    jdbc

--- jdbc/src/Runner.java
+++ jdbc/src/Runner.java
@@ -7,69 +7,96 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import beans.Constants;
 import beans.NumLen;
 
 public class Runner {
+	
 	public static void main(String[] args) {
+		
+		final String QUERRY_MAIN = "SELECT ROUND(ABS(X1 - X2)+0.001) AS len, COUNT(*)"
+				+ " AS num FROM coordinates GROUP BY len ORDER BY len ";
+		final String QUERRY_DELETE_LINE = "DELETE FROM frequencies";
+		final String QUERRY_INSERT = "INSERT INTO frequencies(Len, Num) VALUES(?, ?)";
+		final String QUERRY_SELECT = "SELECT * FROM frequencies WHERE len>num";
+		
+		
 		String driverName = "org.gjt.mm.mysql.Driver";
 		String dbUrl = "jdbc:mysql://localhost/segments"; 
 		String user = "use";
 		String password = "111";
-		List<NumLen> lenNums = new ArrayList<>();
-
 		
-		
+		List<NumLen> lenNums = new ArrayList<NumLen>();
+	
 		try {
 			
 			Class.forName(driverName);
 			
-			try(Connection cn = DriverManager.getConnection(dbUrl, user, password)) {
-				
-				String querry = "SELECT ROUND(ABS(X1 - X2)+0.001) AS len, COUNT(ROUND(ABS(X1 - X2)+0.001))"
-						+ " AS num FROM coordinates GROUP BY ROUND(ABS(X1-X2)+0.001) ORDER BY len ";
-				try(Statement st = cn.createStatement()) {
-									
-					try(ResultSet rs = st.executeQuery(querry)) {
-						while (rs.next()) {
-							lenNums.add(new NumLen(rs.getInt(1),rs.getInt(2)));			
-						}
-						st.executeUpdate("DELETE FROM frequencies");
-					}
-				}
-				
-				for(NumLen numLen :lenNums) {
-					System.out.println(numLen);
-				}
-				System.out.println();
-				
-				
-				
-				String sql = "INSERT INTO frequencies(Len, Num) VALUES(?, ?)";
-				try(PreparedStatement preparedStatement = cn.prepareStatement(sql)) {
-					
-					for(NumLen numLen : lenNums) {
-						preparedStatement.setInt(1, numLen.getLen());
-						preparedStatement.setInt(2, numLen.getNum());
-						preparedStatement.executeUpdate();
-					}
-				}
-				
-				try(Statement statement = cn.createStatement()) {
-					String querry2 = "SELECT * FROM frequencies WHERE len>num";
-					try (ResultSet resultSet = statement.executeQuery(querry2))  {
-						while(resultSet.next()) {
-							System.out.println(resultSet.getString("len")+";"+resultSet.getString("num"));
-						}
-					} 
-				}
-				
+			Connection connection = null;
+			Statement statement = null;
+			PreparedStatement preparedStatement = null;
+			ResultSet resultSet = null;	
 			
+			try {
+				connection = DriverManager.getConnection(dbUrl, user, password);
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery(QUERRY_MAIN);
+					while (resultSet.next()) {
+						NumLen numLen = new NumLen(resultSet.getInt(Constants.INDEX_ONE), resultSet.getInt(Constants.INDEX_TWO));
+						lenNums.add(numLen);			
+					}
+				resultSet.close();
 				
+				print(lenNums);
+				
+				statement.executeUpdate(QUERRY_DELETE_LINE);
+				preparedStatement = connection.prepareStatement(QUERRY_INSERT);
+					
+				for(NumLen numLen : lenNums) {
+					preparedStatement.setInt(Constants.INDEX_ONE, numLen.getLen());
+					preparedStatement.setInt(Constants.INDEX_TWO, numLen.getNum());
+					preparedStatement.executeUpdate();
+				}
+								
+				resultSet = statement.executeQuery(QUERRY_SELECT);
+				while(resultSet.next()) {
+					System.out.println(resultSet.getString(Constants.INDEX_ONE)+";"+resultSet.getString(Constants.INDEX_TWO));
+				}
+			} finally {
+								
+				if(resultSet!=null) {
+					resultSet.close();
+				} else {
+					throw new SQLException(Constants.ERROR_READING);
+				} 
+				if(preparedStatement!=null) {
+					preparedStatement.close();
+				} else {
+					throw new SQLException(Constants.ERROR_STATEMENT);
+				}
+				if(statement!=null) {
+					statement.close();
+				} else {
+					throw new SQLException(Constants.ERROR_STATEMENT);
+				}
+				if(connection!=null) {
+					connection.close();
+				} else {
+					throw new SQLException(Constants.ERROR_CONNECTION);
+				}
 			}		
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+
+	private static void print(List<NumLen> lenNums) {
+		for(NumLen numLen :lenNums) {
+			System.out.println(numLen);
+		}
+		System.out.println();
 	}
 }
--- jdbc/src/beans/Constants.java
+++ jdbc/src/beans/Constants.java
@@ -7,8 +7,9 @@ public class Constants {
 	public static final int INDEX_THREE=3;
 	public static final int INDEX_FOUR=4;
 	
-	
-	
+	public static final String ERROR_READING="Error reading data from the database";
+	public static final String ERROR_STATEMENT = "Statement not created";
+	public static final String ERROR_CONNECTION = "Connection not created";
 	
 	
 }
